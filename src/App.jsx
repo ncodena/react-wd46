a691bda62c824d057ae1f0c680182a1591b18dfa
ncodena <c.odena.nuria@gmail.com>
2024-02-06 08:48:31 +0100

    adding code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
-import Posts from './views/Posts';
-
 
 function App() {
   
@@ -10,7 +8,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path='/posts' element={<Posts/>} />
+
     </Routes>
     </>
   )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,39 +1,58 @@
 import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  //Simple Functions
+  //A simple (or regular) function in JavaScript is a set of statements that performs a task or calculates a value.
 
-    const [jokes, setJokes] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const square = (number) => {
+    return number * number;
+  }
+  
+  //console.log(square(4));
+
+  // Callback Functions
+  // Definition: A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+  //Usage: Callbacks are used for asynchronous operations, such as in event listeners, timers, or making HTTP requests. They're also used in array methods.
+
+  const greeting = (name) => {
+    alert('Hello ' + name);
+  }
+
+  const greetingNúria = () => {
+    alert('Hello Núria');
+  }
+  
+  // const processUserInput = (callback) => {
+  //   const name = prompt('Please enter your name.');
+  //   callback(name);
+  // }
+  
+  // processUserInput(greeting);
+
+  //Map
+
+  const numbers = [1, 4, 5];
+
+  const doubles = numbers.map(number => number * 2)
+  console.log(numbers)
+  console.log(doubles)
+
+  const numbersToFilter = [1,2, 3, 4];
+
+  const evens = numbersToFilter.filter(number => number > 2)
+
+  console.log(evens)
+
+  const findNumberArray = numbersToFilter.find(number => number > 2);
+  console.log(findNumberArray)
 
 
-    useEffect(() => {
-        
-        const fetchJokes = () => {
-            
-          fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit&amount=10')
-          .then((response) => {
-            
-            if(!response.ok){
-                throw new Error('THE FETCH FAILED')
-            }
-
-            return response.json()
-          })
-          .then((data) => setJokes(data))
-          .catch((error) => console.log(error))
-          .finally(() => {
-            setLoading(false)
-          })
-        };
-      
-        fetchJokes();
-      }, []);
-
- 
 
   return (
     <div>
         <h1>Jokes API</h1>
+        <button onClick={() => greeting("Subaru") }>Click me</button>
+        <button onClick={greetingNúria}>Click me Núria</button>
     </div>
   )
 }
